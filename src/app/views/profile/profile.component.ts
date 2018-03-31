c596d9be5b5d86743627abb2f6bfa644c9ce4c8c
Aswin Kumar <aswin.kumar@coda.global>
2018-03-31 05:57:19 +0530

    view history

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -5,7 +5,7 @@ import {
   FadeIn, FadeIn1, FadeIn2,
   LoopAnimation, SlideInFromRight, SizeChange
 } from '../../animation.constants';
-import { ActivatedRoute } from '@angular/router'; 
+import { ActivatedRoute } from '@angular/router';
 import { HttpService } from '../../services/http/http.services';
 @Component({
   selector: 'app-profile',
@@ -13,7 +13,7 @@ import { HttpService } from '../../services/http/http.services';
   styleUrls: ['./profile.component.scss'],
   animations: [CardAnimation1, CardAnimation2, CardAnimation3, CardAnimation4, CardAnimation5, CardAnimation6, FadeIn, FadeIn1, FadeIn2,
     LoopAnimation, SlideInFromRight, SizeChange
-]
+  ]
 })
 export class ProfileComponent implements OnInit {
 
@@ -41,17 +41,18 @@ export class ProfileComponent implements OnInit {
   progressStage2: any;
   progressStage3: any;
   progressStage4: any;
-  teacherId:any;
-  studentId:any;
+  teacherId: any;
+  studentId: any;
   predictedCourse: any;
-  userRole:any;
+  userRole: any;
+  predictionDetails: any;
   constructor(private service: HttpService, private route: ActivatedRoute) {
-    
-   }
+
+  }
 
   ngOnInit() {
     this.currentTab = 'tab1';
-    
+
     this.route.params.subscribe((params) => {
       this.studentId = params['id'];
       this.teacherId = params['teacherId'];
@@ -61,193 +62,203 @@ export class ProfileComponent implements OnInit {
       this.getStudentQuestions(params['id']);
       this.getTeacherQuestions(params['id']);
     });
+
+    this.predictionDetails = [{
+      date: "31.03.2018",
+      status: "dropout"
+    }
+      ,
+    {
+      date: "29.03.2018",
+      status: "dropout"
+    }]
   }
 
-  switchTab(tab){
+  switchTab(tab) {
     this.currentTab = tab;
   }
 
-  saveTeacherAnswer(answer, question){
-    if(this.lastQuestionTeacherIndex <= 5){
+  saveTeacherAnswer(answer, question) {
+    if (this.lastQuestionTeacherIndex <= 5) {
       this.postTeacherAnswer(answer, question.question_id);
     }
     question.question_answer = answer;
   }
 
-  pushNewTeacherQuestion(answer,question ){
-    if(this.lastQuestionTeacherIndex <= 5){
-    this.postTeacherAnswer(answer, question.question_id);
+  pushNewTeacherQuestion(answer, question) {
+    if (this.lastQuestionTeacherIndex <= 5) {
+      this.postTeacherAnswer(answer, question.question_id);
     }
     question.question_answer = answer;
   }
- 
 
-  postTeacherAnswer(answer,questionId ){
+
+  postTeacherAnswer(answer, questionId) {
     this.questions_teacher.push(this.teacherQuestions[++this.lastQuestionTeacherIndex]);
     let request = {
-      teacher_id:this.teacherId,
-      student_id:this.studentId,
-      question_id:questionId,
-      question_answer:answer
+      teacher_id: this.teacherId,
+      student_id: this.studentId,
+      question_id: questionId,
+      question_answer: answer
     };
     console.log(request);
-    this.service.post('/teacher/questions/save',JSON.stringify(request)).then((data)=>{
+    this.service.post('/teacher/questions/save', JSON.stringify(request)).then((data) => {
       console.log(data);
-    }).catch((error)=>{
+    }).catch((error) => {
 
     });
   }
-  saveAnswer(answer, question){
-    if(this.lastQuestionIndex <=5){
+  saveAnswer(answer, question) {
+    if (this.lastQuestionIndex <= 5) {
       this.postStudentAnswer(answer, question.question_id);
     }
-   
+
     question.question_answer = answer;
   }
 
-  postStudentAnswer(answer,questionId ){
+  postStudentAnswer(answer, questionId) {
     this.questions.push(this.studentQuestions[++this.lastQuestionIndex]);
     let request = {
-      student_id:this.studentId,
-      question_id:questionId,
-      question_answer:answer
+      student_id: this.studentId,
+      question_id: questionId,
+      question_answer: answer
     };
     console.log(request);
-    this.service.post('/student/questions/save',JSON.stringify(request)).then((data)=>{
+    this.service.post('/student/questions/save', JSON.stringify(request)).then((data) => {
       console.log(data);
-    }).catch((error)=>{
+    }).catch((error) => {
 
     });
   }
- 
-  pushNewQuestion(answer, question){
-    if(this.lastQuestionIndex <=5){
+
+  pushNewQuestion(answer, question) {
+    if (this.lastQuestionIndex <= 5) {
       this.postStudentAnswer(answer, question.question_id);
     }
-    
+
     question.question_answer = answer;
     // console.log(this.questions);
- 
+
     /* this.questions.push({
       question:'Did you choose this course with passion?',
       answer:'yes'
     }) */
   }
 
-    getStudentDetails(id){
-      this.service.get('/student/'+id).then((data)=>{
-        console.log(data);
-        this.profileDetails = data;
-        this.isLoaded = true;
-      }).catch((error)=>{
+  getStudentDetails(id) {
+    this.service.get('/student/' + id).then((data) => {
+      console.log(data);
+      this.profileDetails = data;
+      this.isLoaded = true;
+    }).catch((error) => {
 
-      });
-    }
+    });
+  }
 
-    getSemesterDetails(id){
-      this.service.get('/student/marks/'+id).then((data)=>{
-        console.log(data);
-        this.semesterDetails = data;
-       // this.isLoaded = true;
-      }).catch((error)=>{
+  getSemesterDetails(id) {
+    this.service.get('/student/marks/' + id).then((data) => {
+      console.log(data);
+      this.semesterDetails = data;
+      // this.isLoaded = true;
+    }).catch((error) => {
 
-      });
-    }
+    });
+  }
 
-    getStudentQuestions(id){
-      this.service.get('/student/questions/'+id).then((data)=>{
-        console.log(data);
-        this.studentQuestions = data;
-       // this.isLoaded = true;
-      
-       // for(let question of this.studentQuestions){
-        for(let i = 0; i < this.studentQuestions.length;i++){
+  getStudentQuestions(id) {
+    this.service.get('/student/questions/' + id).then((data) => {
+      console.log(data);
+      this.studentQuestions = data;
+      // this.isLoaded = true;
+
+      // for(let question of this.studentQuestions){
+      for (let i = 0; i < this.studentQuestions.length; i++) {
         let question = this.studentQuestions[i];
-         if(question.question_answer != null){
+        if (question.question_answer != null) {
           this.questions.push(question);
-         }else{
+        } else {
           this.questions.push(question);
           this.lastQuestionIndex = i;
           break;
-         }
-       };
-      }).catch((error)=>{
+        }
+      };
+    }).catch((error) => {
 
-      });
-    }
+    });
+  }
 
-    getTeacherQuestions(id){
-      this.service.get('/teacher/questions/'+this.teacherId+"?student="+this.studentId).then((data)=>{
-        console.log(data);
-        this.teacherQuestions = data;
-       // this.isLoaded = true;
-      
-       // for(let question of this.studentQuestions){
-        for(let i = 0; i < this.teacherQuestions.length;i++){
+  getTeacherQuestions(id) {
+    this.service.get('/teacher/questions/' + this.teacherId + "?student=" + this.studentId).then((data) => {
+      console.log(data);
+      this.teacherQuestions = data;
+      // this.isLoaded = true;
+
+      // for(let question of this.studentQuestions){
+      for (let i = 0; i < this.teacherQuestions.length; i++) {
         let question = this.teacherQuestions[i];
-         if(question.question_answer != null){
+        if (question.question_answer != null) {
           this.questions_teacher.push(question);
-         }else{
+        } else {
           this.questions_teacher.push(question);
           this.lastQuestionTeacherIndex = i;
           break;
-         }
-       };
-      }).catch((error)=>{
+        }
+      };
+    }).catch((error) => {
 
-      });
-    }
+    });
+  }
 
-    showPrediction(){
-      this.progressStage1 = true;
-      this.progressStage2 = false;
+  showPrediction() {
+    this.progressStage1 = true;
+    this.progressStage2 = false;
+    this.progressStage3 = false;
+    this.progressStage4 = false;
+    setTimeout(() => {
+      this.progressStage1 = false;
+      this.progressStage2 = true;
       this.progressStage3 = false;
       this.progressStage4 = false;
-      setTimeout(() => {
-        this.progressStage1 = false;
-        this.progressStage2 = true;
-        this.progressStage3 = false;
-        this.progressStage4 = false;
-      }, 2000);
-      setTimeout(() => {
-        this.progressStage1 = false;
-        this.progressStage2 = false;
-        this.progressStage3 = true;
-        this.progressStage4 = false;
-      }, 4000);
-      setTimeout(() => {
-        this.progressStage1 = false;
-        this.progressStage2 = false;
-        this.progressStage3 = false;
-        this.progressStage4 = true;
-      }, 6000);
+    }, 2000);
+    setTimeout(() => {
+      this.progressStage1 = false;
+      this.progressStage2 = false;
+      this.progressStage3 = true;
+      this.progressStage4 = false;
+    }, 4000);
+    setTimeout(() => {
+      this.progressStage1 = false;
+      this.progressStage2 = false;
+      this.progressStage3 = false;
+      this.progressStage4 = true;
+    }, 6000);
 
-      this.service.get('/student/questions/'+this.studentId).then((data)=>{
-        console.log(data);
+    this.service.get('/student/questions/' + this.studentId).then((data) => {
+      console.log(data);
       //  this.studentQuestions = data;
       let testData = `{
-        "Q1": [`+ (data[0]['question_answer'] == 'yes' ? 1.0 : 0.0)+`],
-        "Q2": [`+ (data[1]['question_answer'] == 'yes' ? 1.0 : 0.0)+`],
-        "Q3": [`+ (data[2]['question_answer'] == 'yes' ? 1.0 : 0.0)+`],
-        "Q4": [`+ (data[3]['question_answer'] == 'yes' ? 1.0 : 0.0)+`],
-        "Q5": [`+ (data[4]['question_answer'] == 'yes' ? 1.0 : 0.0)+`]  
+        "Q1": [`+ (data[0]['question_answer'] == 'yes' ? 1.0 : 0.0) + `],
+        "Q2": [`+ (data[1]['question_answer'] == 'yes' ? 1.0 : 0.0) + `],
+        "Q3": [`+ (data[2]['question_answer'] == 'yes' ? 1.0 : 0.0) + `],
+        "Q4": [`+ (data[3]['question_answer'] == 'yes' ? 1.0 : 0.0) + `],
+        "Q5": [`+ (data[4]['question_answer'] == 'yes' ? 1.0 : 0.0) + `]  
     }`;
-    this.predictCall(testData);
-    
-      }).catch((error)=>{
+      this.predictCall(testData);
 
-      });
+    }).catch((error) => {
 
-    }
+    });
 
-    predictCall(toSend){
-      this.service.postLocal('http://localhost:5000/predict', (toSend)).then((data)=>{
-     //     student['prediction'] = data['prediction'].toLowerCase();
-        //  console.log('called 8');
-        this.predictedCourse = data['prediction'];
-        }).catch((error)=>{
-    
-        });
-    }
+  }
+
+  predictCall(toSend) {
+    this.service.postLocal('http://localhost:5000/predict', (toSend)).then((data) => {
+      //     student['prediction'] = data['prediction'].toLowerCase();
+      //  console.log('called 8');
+      this.predictedCourse = data['prediction'];
+    }).catch((error) => {
+
+    });
+  }
 
 }
