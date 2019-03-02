9050f145c6e819652284a873d05b9c19d490a6e7
Arvind Muthuraman <arvind.muthuraman@coda.global>
2019-03-03 01:55:12 +0530

    Added institution home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule,ApplicationRef } from '@angular/core';
+import { NgModule, ApplicationRef } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
@@ -11,6 +11,7 @@ import { LayoutComponent } from './layout/layout/layout.component';
 import { LoginComponent } from './views/login/login.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { StudentsListComponent } from './views/students-list/students-list.component';
+import { InstitutionHomepage } from './views/institution-home-page/institution-home-page.component';
 import { DashboardChartComponent } from './components/dashboard-chart/dashboard-chart.component';
 import { ProgressBarCircleComponent } from './components/progress-bar-circle/progress-bar-circle.component';
 import { NotificationsService } from 'angular2-notifications';
@@ -18,20 +19,24 @@ import { SimpleNotificationsModule } from 'angular2-notifications';
 import { HttpService } from './services/http/http.services';
 import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
+import { NgxCarouselModule } from 'ngx-carousel';
+
+
 const appRoutes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'app/institution/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {
     path: 'app', component: LayoutComponent, children:
       [
-        { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+        { path: '', redirectTo: 'institution/home', pathMatch: 'full' },
         { path: 'dashboard', component: DashboardComponent },
         { path: 'dashboard/:role/:id', component: DashboardComponent },
-    //    { path: 'profile', component: ProfileComponent },
+        //    { path: 'profile', component: ProfileComponent },
         { path: 'profile/:role/:id', component: ProfileComponent },
         { path: 'students-list', component: StudentsListComponent },
         { path: 'students-list/:role/:id', component: StudentsListComponent },
         { path: 'profile/:role/:id/:teacherId', component: ProfileComponent },
+        { path: 'institution/home', component: InstitutionHomepage }
       ]
   },
   { path: '**', redirectTo: 'login', pathMatch: 'full' }
@@ -46,7 +51,8 @@ const appRoutes: Routes = [
     DashboardComponent,
     StudentsListComponent,
     DashboardChartComponent,
-    ProgressBarCircleComponent
+    ProgressBarCircleComponent,
+    InstitutionHomepage
   ],
   imports: [
     HttpClientModule,
@@ -55,9 +61,10 @@ const appRoutes: Routes = [
     ChartModule,
     BrowserAnimationsModule,
     CommonModule,
+    NgxCarouselModule,
     AgmCoreModule.forRoot({
-      apiKey:'  AIzaSyCMTH2klaSDgxN83v_ZPDHUn3l34i7h3I8  '
-      }),
+      apiKey: '  AIzaSyCMTH2klaSDgxN83v_ZPDHUn3l34i7h3I8  '
+    }),
     SimpleNotificationsModule.forRoot(),
     RouterModule.forRoot(appRoutes, { useHash: true })
   ],
