21c4e9476d3fac3288f3a0bf6d1b431443c3915b
Geetika <geetika16032@cse.ssn.edu.in>
2019-03-03 17:01:27 +0530

    commit

--- src/app/components/pie/pie.component.ts
+++ src/app/components/pie/pie.component.ts
@@ -20,7 +20,7 @@ export class PieComponent implements OnInit {
         title:{
             text:''
         },
-        colors:['#ff5050','#ff5050','#ff5050','#FFFF00','#FFFF00'],
+        colors:['#00ffff','#00ffff','#00ffff','#00ffff','#FFFF00'],
         yAxis: {
             title: {
                 text: 'Percentage(%)',
